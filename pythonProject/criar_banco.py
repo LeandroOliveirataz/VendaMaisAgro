73c154ee1c7d986f7187bab78a5e21e397fbd5fc
Leandro Oliveira <l3andro.oliveira@gmail.com>
2025-02-19 08:05:56 -0300

    Delete pythonProject directory

--- pythonProject/criar_banco.py
+++
@@ -1,5 +0,0 @@
-from vendamais import database
-from pythonProject.vendamais import app
-
-with app.app_context():
-    database.create_all() # Isso criará as tabelas no banco de dados
--- pythonProject/main.py
+++
@@ -1,5 +0,0 @@
-#pagina 1
-from vendamais import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
